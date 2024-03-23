61f963413000f5ccf4a232ae1bf7bf33286a6c83
Abhishek Manral <abhishek.manral2021@vitbhopal.ac.in>
2024-03-23 21:13:08 +0530

    dsds

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 const port = 9000;
 app.use("/", (req, res) => {
-  res.json({ message: "Hello From Express App" });
+  res.json({ message: "Express App" });
 });
 app.get("/myroute", (req, res) => {
   res.json({ message: "This is the vercel route" });
