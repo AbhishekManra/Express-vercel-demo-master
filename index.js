437c943022ebf6208e650b5cfc0babdd72ae9821
Abhishek Manral <abhishek.manral2021@vitbhopal.ac.in>
2024-03-24 12:08:55 +0530

    cxcxc

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 import express from "express";
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+require('dotenv').config()
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+
+
+
 
 const app = express();
 const port = 9000;
-app.use("/myroute", (req, res) => {
-  res.json({ message: "This is the vercel route" });
+app.use("/myroute",async (req, res) => {
+  // For text-only input, use the gemini-pro model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+  
+  const prompt = "Write a story about a magic backpack in short."
+  
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  res.json({ message: text });
 });
+
 app.use("/", (req, res) => {
   res.json({ message: "Express App" });
 });
