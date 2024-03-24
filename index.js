2c5bd52b7cb887ffa25b696408daab8082efdf33
Abhishek Manral <abhishek.manral2021@vitbhopal.ac.in>
2024-03-25 00:50:53 +0530

    gg

--- index.js
+++ index.js
@@ -14,43 +14,43 @@ dotenv.config(); // Load environment variables from .env file
 // app.get("/myroute", async (req, res) => {
 // });
 
-app.post("/", async (req, res) => {
-  try {
-    const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+// app.post("/", async (req, res) => {
+//   try {
+//     const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+//     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-    // Get the prompt from the request body
-    const { prompt } = req.body;
+//     // Get the prompt from the request body
+//     const { prompt } = req.body;
 
-    // Check if the prompt is provided
-    if (!prompt) {
-      return res.status(400).json({ error: "Prompt is required" });
-    }
+//     // Check if the prompt is provided
+//     if (!prompt) {
+//       return res.status(400).json({ error: "Prompt is required" });
+//     }
 
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text();
-    console.log(text);
+//     const result = await model.generateContent(prompt);
+//     const response = await result.response;
+//     const text = response.text();
+//     console.log(text);
 
-    // Send the response after all the operations have completed
-    res.json({ message: text });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "An error occurred" });
-  }
-});
-
-// app.get("/", async (req, res) => {
-//   const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-//   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-//   const prompt = "Write a story about a magic backpack in short.";
-//   const result = await model.generateContent(prompt);
-//   const response = await result.response;
-//   const text = response.text();
-
-//   // Send the response after all the operations have completed
-//   res.json({ message: text });
+//     // Send the response after all the operations have completed
+//     res.json({ message: text });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "An error occurred" });
+//   }
 // });
+
+app.get("/", async (req, res) => {
+  const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+  const prompt = "Write a story about a magic backpack in short.";
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+
+  // Send the response after all the operations have completed
+  res.json({ message: text });
+});
 
 app.listen(9000, () => {
   console.log(`Starting Server on Port ${port}`);
