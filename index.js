767519a5ccf53afd2feaf99a70cac5a033d1e027
Abhishek Manral <abhishek.manral2021@vitbhopal.ac.in>
2024-03-24 12:13:51 +0530

    final

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-require('dotenv').config()
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
