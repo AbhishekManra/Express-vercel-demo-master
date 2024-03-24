69d9605983fe98bd59de119ea5a1fdea4d4b861d
Abhishek Manral <abhishek.manral2021@vitbhopal.ac.in>
2024-03-24 12:43:15 +0530

    wpllplp

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ dotenv.config(); // Load environment variables from .env file
 // });
 
 app.get("/", async(req, res) => {
-  console.log(req);
+  res.json({ message: "text" });
   const genAI = new GoogleGenerativeAI(process.env.API_KEY);
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
   const prompt = "Write a story about a magic backpack in short.";
   const result = await model.generateContent(prompt);
   const response = await result.response;
   const text = response.text();
-  res.json({ message: text });
+  console.log(text);
 });
 
 app.listen(9000, () => {
