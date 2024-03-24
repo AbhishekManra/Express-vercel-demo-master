f56d3b4fb6c51f6c888730879efaf0dd9b3caddd
Abhishek Manral <abhishek.manral2021@vitbhopal.ac.in>
2024-03-24 12:37:10 +0530

    wer

--- index.js
+++ index.js
@@ -8,19 +8,15 @@ const port = 9000;
 dotenv.config(); // Load environment variables from .env file
 
 // Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 app.get("/myroute", async (req, res) => {
-  // For text-only input, use the gemini-pro model
+  const genAI = new GoogleGenerativeAI(process.env.API_KEY);
   console.log(req);
-  res.json({message:"waiting"});
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-
   const prompt = "Write a story about a magic backpack in short.";
   const result = await model.generateContent(prompt);
   const response = await result.response;
   const text = response.text();
-
   res.json({ message: text });
 });
 
