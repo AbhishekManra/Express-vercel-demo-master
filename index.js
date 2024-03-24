e7d0e57f035457ef60adad16453b718b0789ea64
Abhishek Manral <abhishek.manral2021@vitbhopal.ac.in>
2024-03-24 12:19:39 +0530

    fqzzzz

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
 import express from "express";
 import { GoogleGenerativeAI } from "@google/generative-ai";
-
 import * as dotenv from 'dotenv';
 
-dotenv.config();
+const app = express();
+const port = 9000;
 
+dotenv.config(); // Load environment variables from .env file
 
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
-
-
-
-
-const app = express();
-const port = 9000;
-app.use("/myroute",async (req, res) => {
+app.use("/myroute", async (req, res) => {
   // For text-only input, use the gemini-pro model
-  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
-  
-  const prompt = "Write a story about a magic backpack in short."
-  
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+  const prompt = "Write a story about a magic backpack in short.";
   const result = await model.generateContent(prompt);
   const response = await result.response;
   const text = response.text();
+
   res.json({ message: text });
 });
 
